2236cc96ac1bb2d253b83c2551bb2540b0d37986
FabioMijango <00156022@uca.edu.sv>
2024-11-01 00:47:16 -0600

    Cambios de layer, poco mas...

--- Desing/World/pathScript.gd
+++ Desing/World/pathScript.gd
@@ -1,8 +1,6 @@
 extends TileMapLayer
 
-
 @onready var decoration: TileMapLayer = $"../Decoration"
-
 
 func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
 		if coords in decoration.get_used_cells_by_id(0):
